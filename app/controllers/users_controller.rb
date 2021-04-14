fabdea29b5e1880f1a70ce0ee614cba72620a23b
akira_0924 <80752599+akira-0924@users.noreply.github.com>
2021-04-15 01:10:11 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
         @user = User.find(params[:id]) 
         @checks = Check.where(user_id:current_user.id)
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        if current_user.update(user_params)
+          redirect_to user_path(current_user)
+        else
+          render :edit
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:id, :image, :profile)
+      end
 end
